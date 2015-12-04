d48a033edaf724d106247db6e1adfe0ae896fcf4
il-isidis <il-isidis>
2015-12-04 10:04:13 +0100

    first commit

---
+++ src/main/java/com/mohamed/IlGitApplication.java
@@ -0,0 +1,12 @@
+package com.mohamed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IlGitApplication.class, args);
+    }
+}
---
+++ src/test/java/com/mohamed/IlGitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mohamed;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = IlGitApplication.class)
+public class IlGitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
